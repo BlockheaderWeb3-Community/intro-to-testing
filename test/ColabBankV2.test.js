d1f418d9bfe1bb75ce5f40341bae6b7381982354
ujez <ujezujez@gmail.com>
2022-11-19 22:41:30 +0100

    small inputs

--- test/ColabBankV2.test.js
+++ test/ColabBankV2.test.js
@@ -29,16 +29,57 @@ describe("ColabBank Test Suite", async () => {
     
     describe("Deposits", async () => {
         it("should successfully deposit", async () => {
+            const { colabBank, addr1 } = await loadFixture(deployOneYearLockFixture);
+            
+            const addr1BalanceBeforeDeposit = await colabBank.balances(addr1.address)
+            console.log("addr1 bal before__", addr1BalanceBeforeDeposit)
+            expect(addr1BalanceBeforeDeposit).to.eq(0)
+            expect(colabBank.connect(addr1).deposit(0)).to.be.reverted
+           const addr1DepositTxn =  await colabBank.connect(addr1).deposit(5)
+            
+            const addr1BalanceAfterDeposit = await colabBank.balances(addr1.address)
+            expect(addr1BalanceAfterDeposit).to.eq(5)
+            console.log("addr1 bal after__", addr1BalanceAfterDeposit)
+            
+            const totalColabBalance = await colabBank.totalColabBalance()
+            console.log("total colab balance__", totalColabBalance)
+            expect(totalColabBalance).to.eq(5)
+
+            await expect(addr1DepositTxn).to.emit(colabBank, "Deposit").withArgs(5, anyValue, addr1.address)
+
     
         })
     })
     
     describe("Withdrawals", async () => {
         // write PoC for the vulnerability
-        it("", async () => {
+        it("Should withdraw from ColabBank", async () => {
+            const { colabBank, addr2 } = await loadFixture(deployOneYearLockFixture);
+            
+            const addr1BalanceBeforeWithdrawal = await colabBank.balances(addr2.address)
+            console.log("addr1 bal before__", addr1BalanceBeforeWithdrawal)
+            expect(Number(addr1BalanceBeforeWithdrawal)).to.eq(5)
+            expect(colabBank.connect(addr2).withdraw(5)).to.be.reverted
+           const addr1WithdrawalTxn =  await colabBank.connect(addr2).withdraw(5)
 
+           const addr1BalanceAfterWithdrawal = await colabBank.balances(addr2.address)
+            expect(Number(addr1BalanceAfterWithdrawal)).to.eq(0)
+            console.log("addr1 bal after__", addr1BalanceAfterWithdrawal)
+            
+            const totalColabBalance = await colabBank.totalColabBalance()
+            console.log("total colab balance__", totalColabBalance)
+            expect(Number(totalColabBalance)).to.eq(0)
+
+            await expect(Number(addr1WithdrawalTxn)).to.emit(colabBank, "Withdraw").withArgs(5, anyValue, addr2.address)
     
         })
     })
+
+    //GETTING THIS OUTPUT
+    // AssertionError: expected +0 to equal 5
+    // + expected - actual
+
+    // -0
+    // +5
 
 })
