72e7819e72ca7d8a1382f22a50c9108f2a65bb4c
Faith M. Roberts <faithroberts708@gmail.com>
2022-11-28 19:18:51 +0100

    correcting withdrawal description

--- test/ColabBank.test.js
+++ test/ColabBank.test.js
@@ -48,67 +48,65 @@ describe("ColabBank Test Suite", async () => {
             const { colabBank, addr1 } = await loadFixture(deployOneYearLockFixture);
             
             const addr1BalanceBeforeDeposit = await colabBank.balances(addr1.address)
-            console.log("addr1 bal before__", addr1BalanceBeforeDeposit)
             expect(addr1BalanceBeforeDeposit).to.eq(0)
             expect(colabBank.connect(addr1).deposit(0)).to.be.reverted
-           const addr1DepositTxn =  await colabBank.connect(addr1).deposit(5)
+            
+            const addr1DepositTxn =  await colabBank.connect(addr1).deposit(5)
             
             const addr1BalanceAfterDeposit = await colabBank.balances(addr1.address)
             expect(addr1BalanceAfterDeposit).to.eq(5)
-            console.log("addr1 bal after__", addr1BalanceAfterDeposit)
             
             const totalColabBalance = await colabBank.totalColabBalance()
-            console.log("total colab balance__", totalColabBalance)
             expect(totalColabBalance).to.eq(5)
 
             await expect(addr1DepositTxn).to.emit(colabBank, "Deposit").withArgs(5, anyValue, addr1.address)
-        })
-    })
+        });
+    });
    // Adding the withdrawal test (Faith M. Roberts)
-   describe("Withdrawal", async () => {
-     it("should fail if called before the present time", async () => {
-    const { colabBank, addr1, unlockTime, owner, lockedAmount } = await loadFixture(deployOneYearLockFixture);
+    describe("Withdrawal", async () => {
+        it("should return the correct error message if called before the unlock time", async () => {
+            const { colabBank, addr1, unlockTime, owner, lockedAmount } = await loadFixture(deployOneYearLockFixture);
 
-    const prevColabBalance = await ethers.provider.getBalance(colabBank.address)
-    expect(prevColabBalance).to.eq(lockedAmount)
+            const prevColabBalance = await ethers.provider.getBalance(colabBank.address)
+            expect(prevColabBalance).to.eq(lockedAmount)
 
-    const prevOwnerBalance = await ethers.provider.getBalance(owner.address);
-    expect(prevOwnerBalance).to.be.gte(parseEther('9900'))
+            const prevOwnerBalance = await ethers.provider.getBalance(owner.address);
+            expect(prevOwnerBalance).to.be.gte(parseEther('9900'))
 
-    expect(colabBank.connect(owner).withdraw()).to.be.revertedWith("You can't withdraw yet");
-    await time.increaseTo(unlockTime);
+            expect(colabBank.connect(owner).withdraw()).to.be.revertedWith("You can't withdraw yet");
+            await time.increaseTo(unlockTime);
 
-    // // We use colabBank.connect() to send a transaction from another account
-    await expect(colabBank.connect(addr1).withdraw()).to.be.revertedWith(
-      "You aren't the owner"
-    );
+            // // We use colabBank.connect() to send a transaction from another account
+            await expect(colabBank.connect(addr1).withdraw()).to.be.revertedWith(
+              "You aren't the owner"
+            );
 
 
     // withdraw all ETH in colabBank conttract
-    const ownerWithdrawalTxn = await colabBank.connect(owner).withdraw()
-    await ownerWithdrawalTxn.wait()
+            const ownerWithdrawalTxn = await colabBank.connect(owner).withdraw()
+            await ownerWithdrawalTxn.wait()
 
-    // check owner ETH balance
-    const currentOwnerBalance = await ethers.provider.getBalance(owner.address);
-    expect(currentOwnerBalance).to.be.gt(prevOwnerBalance) 
+            // check owner ETH balance
+            const currentOwnerBalance = await ethers.provider.getBalance(owner.address);
+            expect(currentOwnerBalance).to.be.gt(prevOwnerBalance) 
 
-    const currentColabBalance = await ethers.provider.getBalance(colabBank.address)
+            const currentColabBalance = await ethers.provider.getBalance(colabBank.address)
 
-    expect(currentColabBalance).to.eq(0)
-  });
-});
+            expect(currentColabBalance).to.eq(0)
+        });
+    });
 
     describe("Events", function () {
-    it("Should emit an event on withdrawals", async function () {
-    const { colabBank, unlockTime, lockedAmount} = await loadFixture(
-      deployOneYearLockFixture
-    );
+        it("Should emit an event on withdrawals", async function () {
+            const { colabBank, unlockTime, lockedAmount} = await loadFixture(
+              deployOneYearLockFixture
+            );
 
-    await time.increaseTo(unlockTime);
+            await time.increaseTo(unlockTime);
 
-    await expect(colabBank.withdraw())
-      .to.emit(colabBank, "Withdrawal")
-      .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+            await expect(colabBank.withdraw())
+              .to.emit(colabBank, "Withdrawal")
+              .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+        });
     });
-  });
-})
+});
